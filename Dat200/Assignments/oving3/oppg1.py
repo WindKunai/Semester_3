10677809a6ecb6c003996406131b6a53a4353e7f
WindKunai <117094794+WindKunai@users.noreply.github.com>
2023-09-18 12:49:09 +0200

    Update oppg1.py

--- Dat200/Assignments/oving3/oppg1.py
+++ Dat200/Assignments/oving3/oppg1.py
@@ -21,14 +21,13 @@ def is_palindrome_recursive(word):
     # Base case: if the word is empty or has only one character, it's a palindrome
     if len(word) <= 1:
         return True
-
-    # Check if the first and last characters are the same
+    
     if word[0] == word[-1]:
-        # Recursively check the substring without the first and last characters
+        
         return is_palindrome_recursive(word[1:-1])
 
     return False
 
-# Test the function
-print("The word hello being a palindrome is:", is_palindrome_recursive("hello"))  # Output: False
-print("The word racecar being a palindrome is:", is_palindrome_recursive("racecar"), "\n")  # Output: True
+
+print("The word hello being a palindrome is:", is_palindrome_recursive("hello"))  
+print("The word racecar being a palindrome is:", is_palindrome_recursive("racecar"), "\n") 
